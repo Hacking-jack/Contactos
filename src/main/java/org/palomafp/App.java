8ade241699b118bfd86fb14386bf73c46e77f98c
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2023-10-03 12:38:26 +0200

    Actualización del Main

--- src/main/java/org/palomafp/App.java
+++ src/main/java/org/palomafp/App.java
@@ -1,5 +1,7 @@
 package org.palomafp;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Helo World!" );
+    	int accion = 0;
+    	do{
+    	accion = menu(); 	
+    	}while(accion != 0);
+    }
+    
+    public static int menu() {
+    	System.out.println("¿Que quieres hacer?");
+    	System.out.println(
+    			"1 " + "Buscar por código de usuario" + "\n" +
+    			"2 " + "Buscar por el comienzo del nombre" +  "\n" +
+    			"3 " + "Mostrar la agenda completa" + "\n" +
+    			"4 " + "Añadir un contacto" + "\n" +
+    			"5 " + "Borrar un contacto" + "\n" 
+    			);
+    	int num = 0;
+    	switch (num) {
+    	
+    	case (1) :
+    		
+    	case (2) :
+    		
+    	case (3) :
+    		
+    	case (4) :
+    		
+    	case (5) :
+    		
+    	default :
+    		System.out.println("Fin del programa");
+    	}
+    	return num;
     }
 }
